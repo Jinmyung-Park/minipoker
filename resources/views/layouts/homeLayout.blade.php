8c29a598f2b067e35b7c50edc667f861160054d0
jinmyung.park <jinmyung.park@weserve.co.jp>
2021-12-28 08:41:46 +0000

    heroku setting

--- resources/views/layouts/homeLayout.blade.php
+++ resources/views/layouts/homeLayout.blade.php
@@ -2,7 +2,7 @@
  <div class="bg-light m-auto">
     <div class="text-center mt-2">
         <br />
-        <h1 class="col-sm-6 offset-sm-2 border-bottom pb-3 m-auto font-italic">Original Service</h1>
+        <h1 class="col-sm-6 offset-sm-2 border-bottom pb-3 m-auto font-italic">Mini Poker</h1>
 
         <img class="logo mt-5" src="{{ asset('images/title.png') }}" alt="title">      {{-- title logo --}}
     </div>
