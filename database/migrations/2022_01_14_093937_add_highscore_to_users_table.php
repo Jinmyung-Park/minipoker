dec50bbacbe8705fff7fb01e5126b88def156845
jinmyung.park <jinmyung.park@weserve.co.jp>
2022-01-14 02:06:49 +0000

    migration add

--- database/migrations/2022_01_14_093937_add_highscore_to_users_table.php
+++ database/migrations/2022_01_14_093937_add_highscore_to_users_table.php
@@ -16,6 +16,33 @@ class AddHighscoreToUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->unsignedBigInteger('highscore');
         });
+        
+        $ranking = DB::select("SELECT name,stagechecks.highscore
+                               FROM users
+                               INNER JOIN stagechecks
+                               ON users.id = stagechecks.user_id
+                               ORDER BY highscore DESC");
+                               
+        $userCount = count($ranking);
+        
+        for($i = 0 ; $i<=$userCount-1 ; $i++){
+            $highScoreM = $ranking[$i]->name;
+
+            $highScoreData = DB::select("SELECT name,users.highscore
+                            FROM users
+                            WHERE users.name = '$highScoreM'
+                            ");
+
+            $test = DB::select("SELECT name,stagechecks.highscore
+                               FROM users
+                               INNER JOIN stagechecks
+                               ON users.id = stagechecks.user_id
+                               WHERE users.name = '$highScoreM'
+                                ");
+            
+            DB::table("users")->where('name', $test[0]->name)->update(['highscore' => $test[0]->highscore]);
+
+        }
     }
 
     /**
@@ -26,7 +53,7 @@ class AddHighscoreToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table ->unsignedBigInteger('highscore');
+            $table ->dropColumn('highscore');
         });
     }
 }
